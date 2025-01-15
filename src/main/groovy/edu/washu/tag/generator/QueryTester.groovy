a6b9a9fb95335225621c7a48ba6ffcda417b2a75
Charlie Moore <moore.c@wustl.edu>
2025-01-15 11:59:03 -0600

    Fix spark connection

--- src/main/groovy/edu/washu/tag/generator/QueryTester.groovy
+++ src/main/groovy/edu/washu/tag/generator/QueryTester.groovy
@@ -8,12 +8,13 @@ class QueryTester {
     static void main(String[] args) {
         final SparkSession spark = SparkSession.builder()
             .appName('TestClient')
+            .master('local')
             .withExtensions({ new DeltaSparkSessionExtension() })
             .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
             .config('spark.hadoop.fs.s3a.access.key', 'admin')
-            .config("spark.hadoop.fs.s3a.secret.key", "password") \
-            .config("spark.hadoop.fs.s3a.endpoint", "http://10.27.107.3:9000") \
-            .config("spark.hadoop.fs.s3a.endpoint.region", "us-east-1") \
+            .config('spark.hadoop.fs.s3a.secret.key', 'password') \
+            .config('spark.hadoop.fs.s3a.endpoint', 'http://10.27.107.3:9000') \
+            .config('spark.hadoop.fs.s3a.endpoint.region', 'us-east-1') \
             .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
             .getOrCreate()
 
