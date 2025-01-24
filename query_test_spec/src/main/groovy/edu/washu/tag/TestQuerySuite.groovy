557da5eaf69cf7ba2844a2384769afb0a7179c54
Charlie Moore <moore.c@wustl.edu>
2025-01-24 14:58:20 -0600

    Adds builder for query container

--- query_test_spec/src/main/groovy/edu/washu/tag/TestQuerySuite.groovy
+++ query_test_spec/src/main/groovy/edu/washu/tag/TestQuerySuite.groovy
@@ -1,5 +1,9 @@
 package edu.washu.tag
 
+import groovy.transform.builder.Builder
+import groovy.transform.builder.SimpleStrategy
+
+@Builder(builderStrategy = SimpleStrategy, prefix = '')
 class TestQuerySuite<X extends QuerySourceData> {
 
     String viewName
