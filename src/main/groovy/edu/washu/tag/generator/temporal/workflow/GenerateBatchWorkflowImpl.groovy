c28b373e300a50fd7b5097a74276edc83b9b502f
Charlie Moore <moore.c@wustl.edu>
2025-03-10 18:07:58 -0500

    Exploring temporal

--- src/main/groovy/edu/washu/tag/generator/temporal/workflow/GenerateBatchWorkflowImpl.groovy
+++ src/main/groovy/edu/washu/tag/generator/temporal/workflow/GenerateBatchWorkflowImpl.groovy
@@ -1,20 +1,14 @@
 package edu.washu.tag.generator.temporal.workflow
 
-import edu.washu.tag.generator.BatchProcessor
-import edu.washu.tag.generator.BatchRequest
-import edu.washu.tag.generator.BatchSpecification
-import edu.washu.tag.generator.IdOffsets
-import edu.washu.tag.generator.PopulationGenerator
-import edu.washu.tag.generator.SpecificationParameters
-import edu.washu.tag.generator.YamlObjectMapper
+import edu.washu.tag.generator.*
 import edu.washu.tag.generator.metadata.NameCache
 import edu.washu.tag.generator.temporal.GenerateBatchOutput
 import edu.washu.tag.generator.temporal.TemporalApplication
-import io.temporal.spring.boot.ActivityImpl
+import io.temporal.spring.boot.WorkflowImpl
 import io.temporal.workflow.Workflow
 import org.slf4j.Logger
 
-@ActivityImpl(taskQueues = TemporalApplication.CHILD_QUEUE)
+@WorkflowImpl(taskQueues = TemporalApplication.CHILD_QUEUE)
 class GenerateBatchWorkflowImpl implements GenerateBatchWorkflow {
 
     private static final Logger logger = Workflow.getLogger(GenerateBatchWorkflowImpl)
