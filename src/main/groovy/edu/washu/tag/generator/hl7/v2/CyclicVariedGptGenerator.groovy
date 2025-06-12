8d54b45f5c088335e3184fbfee3bee7921954a1c
Charlie Moore <moore.c@wustl.edu>
2025-06-12 09:47:21 -0500

    Early work

--- src/main/groovy/edu/washu/tag/generator/hl7/v2/CyclicVariedGptGenerator.groovy
+++ src/main/groovy/edu/washu/tag/generator/hl7/v2/CyclicVariedGptGenerator.groovy
@@ -1,14 +1,18 @@
 package edu.washu.tag.generator.hl7.v2
 
-import edu.washu.tag.generator.ai.AzureWrapper
 import edu.washu.tag.generator.ai.GeneratedReport
+import edu.washu.tag.generator.ai.OpenAiWrapper
 import edu.washu.tag.generator.metadata.Patient
 
 class CyclicVariedGptGenerator extends CyclicVariedGenerator {
 
+    String endpoint
+    String apiKeyEnvVar
+    String model
+
     @Override
     protected List<GeneratedReport> formBaseReports(Patient patient) {
-        new AzureWrapper().generateReports(patient)
+        new OpenAiWrapper(endpoint, apiKeyEnvVar, model).generateReports(patient)
     }
 
 }
