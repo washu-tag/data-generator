9972bfb8f21deb78a8cc66f348a0d576ff87b66d
Charlie Moore <moore.c@wustl.edu>
2025-09-03 15:47:22 -0500

    Fixed math errors in assigning batches

--- src/main/groovy/edu/washu/tag/generator/BatchChunk.groovy
+++ src/main/groovy/edu/washu/tag/generator/BatchChunk.groovy
@@ -2,8 +2,6 @@ package edu.washu.tag.generator
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 
-import static java.lang.Math.round
-
 class BatchChunk {
 
     int numPatients
@@ -21,22 +19,23 @@ class BatchChunk {
 
     @JsonIgnore
     List<BatchRequest> resolveToBatches() {
-        final int patientsInIncompleteBatch = numPatients % patientsPerFullBatch
-        final boolean hasIncompleteBatch = patientsInIncompleteBatch != 0
-        final int studiesPerFullBatch = round(((long) patientsPerFullBatch * (long) numStudies) / numPatients).intValue()
-        final int studiesInIncompleteBatch = numStudies % studiesPerFullBatch
-        final int seriesPerFullBatch = round(((long) patientsPerFullBatch * (long) numSeries) / numPatients).intValue()
-        final int seriesInIncompleteBatch = numSeries % seriesPerFullBatch
         final int totalNumBatches = calculateNumBatches()
+
+        final int patientsInSmallerBatches = numPatients / totalNumBatches
+        final int remainingPatientsToAllocate = numPatients % totalNumBatches
+        final int studiesInSmallerBatch = numStudies / totalNumBatches
+        final int remainingStudiesToAllocate = numStudies % totalNumBatches
+        final int seriesInSmallerBatch = numSeries / totalNumBatches
+        final int remainingSeriesToAllocate = numSeries % totalNumBatches
+
         (0 ..< totalNumBatches).collect { batchId ->
-            final boolean isPartialBatch = hasIncompleteBatch && batchId == totalNumBatches - 1
             new BatchRequest(
                 id: batchIdOffset + batchId,
-                numPatients: isPartialBatch ? patientsInIncompleteBatch : patientsPerFullBatch,
-                numStudies: isPartialBatch ? studiesInIncompleteBatch : studiesPerFullBatch,
-                numSeries:  isPartialBatch ? seriesInIncompleteBatch : seriesPerFullBatch,
-                patientOffset: patientOffset + patientsPerFullBatch * batchId,
-                studyOffset: studyOffset + studiesPerFullBatch * batchId
+                numPatients: patientsInSmallerBatches + (batchId < remainingPatientsToAllocate ? 1 : 0),
+                numStudies: studiesInSmallerBatch + (batchId < remainingStudiesToAllocate ? 1 : 0),
+                numSeries: seriesInSmallerBatch + (batchId < remainingSeriesToAllocate ? 1 : 0),
+                patientOffset: patientOffset + patientsInSmallerBatches * batchId + Math.min(batchId, remainingPatientsToAllocate),
+                studyOffset: studyOffset + studiesInSmallerBatch * batchId + Math.min(batchId, remainingStudiesToAllocate)
             )
         }
     }
